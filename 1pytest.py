d932ad56a2e37e647b69bee6c1065f59d224e38c
AlekseyGordik <118666128+AlekseyGordik@users.noreply.github.com>
2023-02-01 14:47:08 +0200

    Add files via upload

---
+++ 1pytest.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+import time
+import os
+def test_1():
+    assert 10 + 10 == 20
+absolute_path = os.path.dirname(__file__)
+relative_path = "/"
+full_path = os.path.join(absolute_path, relative_path)
+# print(f'Path: {full_path}')
+# print('Path:'+ full_path)
+
+options = Options()
+options.add_argument("--no-sandbox")
+
+driver = webdriver.Chrome(service=Service("С:\\chromedriver.exe, options=options") )
+
+user = "guest"
+password = "welcome2qauto"
+driver.get("https://" + user + ":" + password + "@" + "qauto2.forstudy.space/")
+# driver.get("https://qauto2.forstudy.space/")
+
+time.sleep(3)  # sleep for 3 sec
+driver.get("https://qauto2.forstudy.space/")
+
+time.sleep(4) #sleep for 4 sec
+
+assert "Hillel" in driver.title
+
+def is_element_present(self, hillel, auto):
+    try:
+        self.driver.find_element(by=hillel, value=auto)
+    except NoSuchElementException as e:
+        return False
+    return True
+
+
+# try:
+#     element = driver.find_element(By.ID, "contactsSection1")
+# except NoSuchElementException:
+#     print("No element found")
+
+element = driver.find_element(By.ID, 'contactsSection')  # this element is visible
+if element.is_displayed():
+    print("Element found")
+    not_found = True
+else:
+    print("Element not found")
+    not_found = False
+
+assert not_found
+
+driver.close()
---
+++ 4 pytest.py
@@ -0,0 +1,31 @@
+import re
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+# from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+def increment(x):
+    return x+5
+def test_4_answer():
+    assert increment(10) <= 18
+
+options = Options()
+# options.add_argument("start-maximized"); # open Browser in maximized mode
+# options.add_argument("disable-infobars"); # disabling infobars
+# options.add_argument("--disable-extensions"); # disabling extensions
+# options.add_argument("--disable-gpu"); # applicable to windows os only
+# options.add_argument("--disable-dev-shm-usage"); # overcome limited resource problems
+options.add_argument("--no-sandbox")
+# options.add_argument("headless")
+
+
+driver = webdriver.Chrome(service=Service("С:\\chromedriver.exe, options=options") )
+
+driver.get("https://www.work.ua/jobs-odesa/")
+
+time.sleep(3) #sleep for 3 sec
+
+assert "Робота в Одесі" in driver.title
